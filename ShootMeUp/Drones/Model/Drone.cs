106b4167e4287c9ac4e16c9e8bdf137083597318
Pt74bsx <pt74bsx@eduvaud.ch>
2026-09-11 16:25:22 +0200

    chore(code): Ajout de la base de code (Drones) disponnible sur pm2 etml

---
+++ ShootMeUp/Drones/Model/Drone.cs
@@ -0,0 +1,70 @@
+﻿using Drones.Helpers;
+using Drones.Properties;
+
+namespace Drones
+{
+    // Cette partie de la classe Drone définit ce qu'est un drone par un modèle numérique
+    public class Drone
+    {
+        public int charge;                            // La charge actuelle de la batterie
+        public string name;                           // Un nom
+        public int x;                                 // Position en X depuis la gauche de l'espace aérien
+        public int y;                                 // Position en Y depuis le haut de l'espace aérien
+        public int speed_x;                           // Déplacement horizontal
+        public int speed_y;
+        // Déplacement vertical
+        private Random _alea = new Random();
+
+        // Constructeur
+        public Drone(int x, int y, string name)
+        {
+            Random alea = new Random();
+            this.x = x;
+            this.y = y;
+            this.name = name;
+            charge = alea.Next(1000); // La charge initiale de la batterie est choisie aléatoirement
+            ChangeDirection();
+        }
+
+        // Cette méthode calcule le nouvel état dans lequel le drone se trouve après
+        // que 'interval' millisecondes se sont écoulées
+        public void Update(int interval)
+        {
+            x += speed_x;   
+            y += speed_y;
+            charge--;
+        }
+
+        // Choisit une nouvelle vitesse aléatoirement
+        public void ChangeDirection()
+        {
+            speed_x = _alea.Next(-3, 4);
+            speed_y = _alea.Next(-3, 4);
+        }
+
+        /// //////////////////////////////////////////////////////////////////////////////
+        //  
+        //  Ce qui suit appartient à la vue, pas au modèle.
+        //  Il aurait été préférable de séparer la déclaration de la classe Drone en deux,
+        //  Nous regroupons tout ici pour simplifier
+        //  
+        /// //////////////////////////////////////////////////////////////////////////////
+
+        private Pen droneBrush = new Pen(new SolidBrush(Color.Purple), 3);
+
+        // De manière graphique
+        public void Render(BufferedGraphics drawingSpace)
+        {
+            drawingSpace.Graphics.DrawImage(Resources.drone, x, y, 50, 50);
+            drawingSpace.Graphics.DrawString($"{this}", TextHelpers.drawFont, TextHelpers.writingBrush, x + 5, y - 25);
+        }
+
+        // De manière textuelle
+        public override string ToString()
+        {
+            return $"{name} ({((int)((double)charge / 1000 * 100)).ToString()}%)";
+        }
+
+
+    }
+}
---
+++ ShootMeUp/Drones/View/AirSpace.cs
@@ -0,0 +1,65 @@
+namespace Drones
+{
+    // La classe AirSpace représente le territoire au dessus duquel les drones peuvent voler
+    // Il s'agit d'un formulaire (une fenêtre) qui montre une vue 2D depuis en dessus
+    // Il n'y a donc pas de notion d'altitude qui intervient
+
+    public partial class AirSpace : Form
+    {
+        public static readonly int WIDTH = 1200;        // Dimensions of the airspace
+        public static readonly int HEIGHT = 600;
+
+        // La flotte est l'ensemble des drones qui évoluent dans notre espace aérien
+        private Drone _player;
+
+        BufferedGraphicsContext currentContext;
+        BufferedGraphics airspace;
+
+        // Initialisation de l'espace aérien avec un certain nombre de drones
+        public AirSpace(Drone player)
+        {
+            InitializeComponent();
+            ClientSize = new Size(WIDTH, HEIGHT);
+
+            // Gets a reference to the current BufferedGraphicsContext
+            currentContext = BufferedGraphicsManager.Current;
+            // Creates a BufferedGraphics instance associated with this form, and with
+            // dimensions the same size as the drawing surface of the form.
+            airspace = currentContext.Allocate(this.CreateGraphics(), this.DisplayRectangle);
+            this._player = player;
+        }
+
+        // Affichage de la situation actuelle
+        private void Render()
+        {
+            airspace.Graphics.Clear(Color.AliceBlue);
+
+            _player.Render(airspace);
+
+            airspace.Render();
+        }
+
+        // Calcul du nouvel état après que 'interval' millisecondes se sont écoulées
+        private void Update(int interval)
+        {
+            _player.Update(interval);
+        }
+
+        // Méthode appelée à chaque frame
+        private void NewFrame(object sender, EventArgs e)
+        {
+            this.Update(ticker.Interval);
+            this.Render();
+        }
+
+        private void AirSpace_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                    _player.ChangeDirection();
+                    break;
+            }
+        }
+    }
+}
